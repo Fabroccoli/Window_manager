0037da0146b98458bb831f8499790e442ba6b130
fabroccoli <fabro.busi@gmail.com>
2024-04-12 22:38:31 +0200

    mes versions dofus window_manaement

---
+++ window_manager_win32.py
@@ -0,0 +1,40 @@
+import keyboard
+import pyautogui
+import win32gui
+import win32con
+
+def focus_window(title):
+    # Simule la pression de touches shift
+    pyautogui.PAUSE = 0  # Définis un délai très court entre les actions
+    pyautogui.press('shift')
+    
+    # Trouver la fenêtre par le titre sans changer son état (ni restaurer, ni maximiser)
+    def window_enum_handler(hwnd, top_windows):
+        if win32gui.IsWindowVisible(hwnd) and title in win32gui.GetWindowText(hwnd):
+            top_windows.append(hwnd)
+
+    top_windows = []
+    win32gui.EnumWindows(window_enum_handler, top_windows)
+    
+    if top_windows:
+        hwnd = top_windows[0]
+        win32gui.SetForegroundWindow(hwnd)  # Met simplement la fenêtre au premier plan
+    else:
+        print(f'Aucune fenêtre trouvée avec le titre : {title}')
+
+def on_key_event(event):
+    key = event.name.upper()
+    if key in window_titles:
+        focus_window(window_titles[key])
+
+window_titles = {
+    'F1': 'Fab-un - Dofus 2.71.3.4',
+    'F2': 'Fab-deux - Dofus 2.71.3.4',
+    'F3': 'Fab-three - Dofus 2.71.3.4',
+    'F4': 'Fab-quattre - Dofus 2.71.3.4'
+}
+
+keyboard.on_press(on_key_event)
+
+while True:
+    pass
