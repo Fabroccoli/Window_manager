9fe24036356e75ed2f6b988623826f9c122caeac
fabroccoli <fabro.busi@gmail.com>
2024-04-19 17:35:19 +0200

    Zaap version flexible

--- Zaap.py
+++ Zaap.py
@@ -1,17 +1,59 @@
 import keyboard
 import pyautogui
 import win32gui
+import json
+
+# Fonction pour charger une configuration depuis un fichier spécifique
+def load_config(filename):
+    try:
+        with open(filename + '.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("Fichier non trouvé.")
+        return None
+
+# Fonction pour sauvegarder la configuration dans un fichier spécifique
+def save_config(window_titles, filename):
+    with open(filename + '.json', 'w') as file:
+        json.dump(window_titles, file, indent=4)
+    print("Configuration sauvegardée avec succès dans", filename + '.json')
+
+# Variable pour suivre si la configuration a été chargée
+config_loaded = False
+
+# Demander à l'utilisateur s'il souhaite charger une configuration existante
+if input("Avez-vous une configuration à charger ? (oui/non) ").lower() == 'oui':
+    filename = input("Entrez le nom du fichier à charger : ")
+    window_titles = load_config(filename)
+    if window_titles:
+        print("Configuration chargée avec succès.")
+        config_loaded = True
+        for key, value in window_titles.items():
+            print(f"{key}: {value}")
+    else:
+        window_titles = {}
+else:
+    window_titles = {}
+
+# Si aucune configuration n'est chargée ou si l'utilisateur souhaite en créer une nouvelle
+if not window_titles:
+    total = int(input("Nombre de perso: "))
+    for n in range(1, total + 1):
+        nom = input(f"Entrer le nom du perso {n} (Attention aux majuscule) : ")
+        touche = input(f"Entrer une touche pour {nom} : ")
+        version = " - Dofus 2.71.4.5"
+        window_titles[touche] = f'{nom}{version}'
+
+# Demander à l'utilisateur s'il souhaite sauvegarder cette configuration, uniquement si aucune config n'a été chargée
+if not config_loaded:
+    do_save = input("Voulez-vous sauvegarder cette configuration ? (oui/non) ")
+    if do_save.lower() == 'oui':
+        filename = input("Entrez le nom du fichier pour sauvegarder : ")
+        save_config(window_titles, filename)
+    else:
+        print("Sauvegarde non effectuée.")
 
 
-# Définition des titres de fenêtre associés à chaque touche
-window_titles = {
-    'F1': 'Fab-un - Dofus 2.71.3.4',
-    'F2': 'Fab-deux - Dofus 2.71.3.4',
-    'F3': 'Fab-three - Dofus 2.71.3.4',
-    'F4': 'Fab-quattre - Dofus 2.71.3.4'
-}
-
-# Fonction pour activer une fenêtre donnée
 def activate_window(title):
     # Simule la pression de la touche Shift rapidement
     pyautogui.press('shift')
